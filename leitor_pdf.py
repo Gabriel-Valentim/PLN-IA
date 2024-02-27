cad9cfcfbc7d5a87d3fca527876f6ae6441395fe
Gabrielvd <gabrieldeoliveira4@gmail.com>
2024-02-27 10:16:44 -0300

    atualizaçoes do leitor de pdf

--- leitor_pdf.py
+++ leitor_pdf.py
@@ -2,10 +2,27 @@ import PyPDF2
 import nltk
 
 nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('wordnet')
+
+from nltk.stem import PorterStemmer, WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+
+lemmatizer = WordNetLemmatizer()
+stemmer = PorterStemmer()
 
 def retira_stop_words(texto):
     
+    words_in_quote = word_tokenize(texto, "english", False)
+    #print("--------")
+    #print(words_in_quote)
+    print("--------")
+    stop_wrds = set(stopwords.words("english"))
+    filtered_list = []
+    
     stop_words = nltk.corpus.stopwords.words("english")
+    #********
     cont = 0
     for palavra in texto.split():
         if palavra.lower() not in stop_words:
@@ -14,6 +31,40 @@ def retira_stop_words(texto):
         if cont%7 == 0:
              arquivo_saida.write("\n")
              cont = 1
+    #*****************
+    
+    #RETIRANDO STOP_WORDS
+    for word in words_in_quote:
+        if word.casefold() not in stop_wrds:
+            filtered_list.append(word.lower())
+    print(filtered_list)
+
+    return filtered_list
+
+
+
+def fazendo_lemmatizing(filtered_list):
+
+    lemmatized_words = []
+    for word in filtered_list:
+        lemmatized_words.append(lemmatizer.lemmatize(word, "v")) #v é para retirar o formato dos verbos
+
+    #lemmatized_words = [lemmatizer.lemmatize(word) for word in filtered_list]
+    
+    #print(lemmatized_words)
+    return lemmatized_words
+    print("--------")
+
+
+def fazendo_stemming(filtered_list):
+
+    stemmed_words = []
+    for words in filtered_list:
+        stemmed_words.append(stemmer.stem(words))
+    
+    return stemmed_words
+    print(stemmed_words)
+    print("--------")
 
 
 arquivo_pdf = open("artigos/artigo.pdf", 'rb')
@@ -28,7 +79,11 @@ texto = pagina.extract_text()  # Extrair o texto da página
 
 print(texto)
 
-retira_stop_words(texto)
+filtered_list = retira_stop_words(texto)
+
+lemmatize_wrd = fazendo_lemmatizing(filtered_list)
+
+#stemmed_wrd = fazendo_stemming(lemmatize_wrd)
 
 arquivo_pdf.close()
 arquivo_saida.close()
