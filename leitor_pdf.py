aa791f6361bb4b908af84968187a8964480f86bb
DaniloArr <danilo.arreb@gmail.com>
2024-02-27 16:50:13 -0300

    alteracoes na lematizacao

--- leitor_pdf.py
+++ leitor_pdf.py
@@ -1,9 +1,11 @@
 import PyPDF2
 import nltk
+import re
 
 nltk.download('stopwords')
 nltk.download('punkt')
 nltk.download('wordnet')
+nltk.download('averaged_perceptron_tagger')
 
 from nltk.stem import PorterStemmer, WordNetLemmatizer
 from nltk.tokenize import word_tokenize
@@ -11,33 +13,23 @@ from nltk.corpus import stopwords
 
 lemmatizer = WordNetLemmatizer()
 stemmer = PorterStemmer()
+arquivo_saida = open("arquivo_saida.txt", 'w')
 
 def retira_stop_words(texto):
     
     words_in_quote = word_tokenize(texto, "english", False)
-    #print("--------")
-    #print(words_in_quote)
-    print("--------")
     stop_wrds = set(stopwords.words("english"))
     filtered_list = []
-    
-    stop_words = nltk.corpus.stopwords.words("english")
-    #********
-    cont = 0
-    for palavra in texto.split():
-        if palavra.lower() not in stop_words:
-            arquivo_saida.write(palavra + " ")
-            cont = cont + 1
-        if cont%7 == 0:
-             arquivo_saida.write("\n")
-             cont = 1
-    #*****************
     
     #RETIRANDO STOP_WORDS
     for word in words_in_quote:
         if word.casefold() not in stop_wrds:
-            filtered_list.append(word.lower())
-    print(filtered_list)
+            filtered_list.append(word.casefold())
+    
+    filtered_list = [re.sub('[,.!?;()]|', '', word) for word in filtered_list]
+
+    while '' in filtered_list:
+        filtered_list.pop(filtered_list.index(''))
 
     return filtered_list
 
@@ -45,15 +37,22 @@ def retira_stop_words(texto):
 
 def fazendo_lemmatizing(filtered_list):
 
+    #funcao para descobrir se a palavra é um verbo, substantivo, ou adjetivo e após isso fazer a lematizacao
+    tagged_words = nltk.pos_tag(filtered_list)
     lemmatized_words = []
-    for word in filtered_list:
-        lemmatized_words.append(lemmatizer.lemmatize(word, "v")) #v é para retirar o formato dos verbos
-
-    #lemmatized_words = [lemmatizer.lemmatize(word) for word in filtered_list]
     
-    #print(lemmatized_words)
+    for word, tag in tagged_words:
+        if "VB" in tag:
+            lemmatized_words.append(lemmatizer.lemmatize(word, "v")) #V é para retirar o formato dos verbos
+        elif "NN" in tag:
+            lemmatized_words.append(lemmatizer.lemmatize(word, "n")) #N é para retirar o formato dos substantivos
+        elif "JJ" in tag:
+            lemmatized_words.append(lemmatizer.lemmatize(word, "a")) #A é para retirar o formato dos adjetivos
+        else:
+             lemmatized_words.append(word)
+
+    print(lemmatized_words)
     return lemmatized_words
-    print("--------")
 
 
 def fazendo_stemming(filtered_list):
@@ -63,27 +62,62 @@ def fazendo_stemming(filtered_list):
         stemmed_words.append(stemmer.stem(words))
     
     return stemmed_words
-    print(stemmed_words)
-    print("--------")
 
 
-arquivo_pdf = open("artigos/artigo.pdf", 'rb')
-arquivo_saida = open("arquivo_saida.txt", 'w')
+def main():
 
-pdf = PyPDF2.PdfReader(arquivo_pdf)
+    nome_artigo = ""
 
-# Acessar a página pelo índice
-pagina = pdf.pages[0]  # Para acessar a primeira página, use o índice 0
+    while True:
+        try:
+            numero_artigo = int(input("Digite o número do artigo (0-9): "))
+            if 0 <= numero_artigo < 10:
+                if numero_artigo == 0:
+                    nome_artigo = "artigos/artigo.pdf"
+                elif numero_artigo == 1:
+                    nome_artigo = "artigos/artigo2.pdf"
+                elif numero_artigo == 2:
+                    nome_artigo = "artigos/artigo3.pdf"
+                elif numero_artigo == 3:
+                    nome_artigo = "artigos/artigo4.pdf"
+                elif numero_artigo == 4:
+                    nome_artigo = "artigos/artigo5.pdf"
+                elif numero_artigo == 5:
+                    nome_artigo = "artigos/artigo6.pdf"
+                elif numero_artigo == 6:
+                    nome_artigo = "artigos/artigo7.pdf"
+                elif numero_artigo == 7:
+                    nome_artigo = "artigos/artigo8.pdf"
+                elif numero_artigo == 8:
+                    nome_artigo = "artigos/artigo9.pdf"
+                elif numero_artigo == 9:
+                    nome_artigo = "artigos/artigo10.pdf"
+                break
+            else:
+                print("Número inválido. Digite um número entre 0 e 9.")
+        except ValueError:
+            print("Entrada inválida. Digite um número inteiro.")
 
-texto = pagina.extract_text()  # Extrair o texto da página
+    arquivo_pdf = open(nome_artigo, 'rb')
+    
 
-print(texto)
+    pdf = PyPDF2.PdfReader(arquivo_pdf)
 
-filtered_list = retira_stop_words(texto)
+    # Acessar a página pelo índice
+    pagina = pdf.pages[0]  # Para acessar a primeira página, use o índice 0
 
-lemmatize_wrd = fazendo_lemmatizing(filtered_list)
+    texto = pagina.extract_text()  # Extrair o texto da página
 
-#stemmed_wrd = fazendo_stemming(lemmatize_wrd)
+    print(texto)
 
-arquivo_pdf.close()
-arquivo_saida.close()
+    filtered_list = retira_stop_words(texto)
+
+    lemmatize_wrd = fazendo_lemmatizing(filtered_list)
+
+    #stemmed_wrd = fazendo_stemming(lemmatize_wrd)
+
+    arquivo_pdf.close()
+    arquivo_saida.close()
+
+if __name__ == "__main__":
+  main()
